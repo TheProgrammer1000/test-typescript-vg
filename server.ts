c4c2c1733048480145351a12de4f166580e3d546
Dennis <112337016+TheProgrammer1000@users.noreply.github.com>
2023-12-14 13:03:55 +0100

    Finalllle

--- server.ts
+++ server.ts
@@ -1,1 +1,8 @@
-let hej = 0;
+import express from 'express'
+
+let port = 8001
+const app = express();// Make app och alla funktioner till den
+
+app.listen(port, () => {
+  console.log('Listning to port 8001');
+})
