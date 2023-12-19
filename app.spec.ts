eed9272fb6cbdf64a88dd4dafab609c8ec2d282f
Dennis <112337016+TheProgrammer1000@users.noreply.github.com>
2023-12-19 11:04:43 +0100

    Testing finish now

--- app.spec.ts
+++ app.spec.ts
@@ -1,14 +1,33 @@
-import nock from 'nock';
-import {default as request} from 'supertest';
-import { makeApp } from './app';
+import {default as request} from 'supertest'
+import { makeApp } from "./app";
 
 const app = makeApp();
 
-describe('check test', () => {
-  it('should return true testing', async () => {
+describe('GET /contact', () => {
+    it('should get all contacts and getting coordinates of that contact', () => {
 
-    const response = await request(app).get('/contact')
-    console.log('response', response)
-    expect(response.statusCode).toBe(200)
-  })
+    })
+})
+
+describe('GET /contact:id', () => {
+    it('should return statusCode 200 on valid contactID', () => {
+
+    })
+
+    it('should return statusCode 404 on invalid contactID', () => {
+
+    })
+})
+
+describe('POST /contact', () => {
+    it('should have all fields with required types', () => {
+
+    })
+
+    it('should return statusCode 201 when valid', () => {
+
+    })
+    it('should return statusCode 400 when invalid', () => {
+
+    })
 })
--- app.ts
+++ app.ts
@@ -1,45 +1,20 @@
-import express, {json} from 'express'
-
-export const makeApp = () => {
-  const app = express();
-  app.use(json())
-
-  app.get('/', (req, res) => {
-    res.json({tja: "tjuuuu"})
-  })
-
-  app.post('/contact', (req, res) => {
-  })
-
-  app.get('/contact', (req, res) => {
-    res.statusCode = 200
-  })
-
-  app.get('/contact/:id', (req, res) => {
-
-  })
-
-
-  return app
-}
-
 import express from 'express';
 
 const app = express();
 
 
-export const myApp = () => {
+export const makeApp = () => {
     app.get('/', (req, res) => {
         res.send('Hello World');
     });
 
-    app.routes.get('contact', () => {
-
-    }).post('contact', () => {});
-
-    app.get('contact/:id', () => {
-
+    app.get('/contact', (req, res) => {
+      res.status(200).send('yk')
     })
+
+    app.post('/contact', (req, res) => {
+
+    });
 
     return app;
 }
